b0685d1fd186b15bf2edb17f4608764c766cea66
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-07-31 22:03:47 +0530

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -17,7 +17,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Image src="/static/images/logo.png" width={50} height={50}></Image>
+                  <Image src="/blog/static/images/logo.png" width={50} height={50}></Image>
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
