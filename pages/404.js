b4163fea3e27a169dd1d09ab27a5871bfd2296a9
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-08-11 11:13:57 +0530

    fixed prettier errors

--- pages/404.js
+++ pages/404.js
@@ -7,14 +7,14 @@ export default function FourZeroFour() {
     <>
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
             404
           </h1>
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
+            Sorry we couldnt find this page.
           </p>
           <p className="mb-8">
             But dont worry, you can find plenty of other things on our homepage.
