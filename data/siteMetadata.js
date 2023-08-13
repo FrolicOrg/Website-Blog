1dfc22188595cd4aaf578e1bc0b1f1c48c04232f
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-08-13 10:05:56 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'Learn about latest development in field of Analytics and AI',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.frolicorg.com',
+  siteRepo: 'https://github.com/frolicorg',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
